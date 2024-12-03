7747183478a48cdad13abd2a99bcd1e5fabe33a2
hieunghia-pat <19520178@gm.uit.edu.vn>
2024-12-03 08:38:47 +0700

    refined the code for Gemini

--- LLMs-evaluation/Gemini/evaluate-task-1-2.py
+++ LLMs-evaluation/Gemini/evaluate-task-1-2.py
@@ -3,10 +3,10 @@ import json
 from tqdm import tqdm
 import time
 
-genai.configure(api_key="AIzaSyD0OoyhPELgVdiXPl_2rilO33xqnEmMQKk")
+genai.configure(api_key="API Key")
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-data = json.load(open("/home/ngiangh/Projects/ViLeQuAD/dataset/test_data.json"))
+data = json.load(open("../test_data.json"))
 results = {}
 total_error_requests = 0
 with tqdm(data) as pb:
@@ -14,7 +14,7 @@ with tqdm(data) as pb:
         item = data[key]
         question = item["question"]
         contexts = item["contexts"]
-        prompt = f"Câu hỏi: {question}. Để trả lời câu hỏi này cần tham chiếu đến loại văn bản pháp luật nào của Việt Nam và cụ thể số hiệu của văn bản đó là gì? Câu trả lời cần ở dạng JSON, với khóa DOCUMENT chỉ loại văn bản pháp luật và NUMBER chỉ số liệu của văn bản đó."
+        prompt = f"Câu hỏi: {question}. Để trả lời câu hỏi này cần tham chiếu đến loại văn bản pháp luật nào của Việt Nam và cụ thể số hiệu của văn bản đó, chương nào, mục nào, tiểu mục nào, điều nào, khoản nào? Câu trả lời cần ở dạng JSON, với khóa DOCUMENT chỉ loại văn bản pháp luật, NUMBER chỉ số liệu của văn bản đó, CHAPTER chỉ chương (nếu có), SECTION chỉ mục (nếu có), SUBSECTION chỉ tiểu mục (nếu có), STATEMENT chỉ điều (nếu có), CLAUSE chỉ khoản (nếu có)."
         reference = []
         for id in contexts:
             context = contexts[id]
@@ -42,5 +42,5 @@ with tqdm(data) as pb:
             "reference": reference
         }
 
-        json.dump(results, open("results-gemini-1.5-flash.json", "w+"), ensure_ascii=False, indent=4)
+        json.dump(results, open("results-gemini-1.5-flash-task-1-2.json", "w+"), ensure_ascii=False, indent=4)
         time.sleep(10)
